d100ae0fe681b151b740120207056763d37aa0f8
aity-yahoo <136758308+aity-yahoo@users.noreply.github.com>
2023-06-16 18:28:50 +0200

    Update adding.py

--- adding.py
+++ adding.py
@@ -11,9 +11,9 @@ import random
 import re
 
 
-api_id =         # YOUR API_ID
-api_hash = ''        # YOUR API_HASH
-phone = ''        # YOUR PHONE NUMBER, INCLUDING COUNTRY CODE
+api_id = 9240450       # YOUR API_ID
+api_hash = 'cf8362a959332eab50c5993158402644'        # YOUR API_HASH
+phone = '380942594577'        # YOUR PHONE NUMBER, INCLUDING COUNTRY CODE
 client = TelegramClient(phone, api_id, api_hash)
 
 client.connect()
